0499041352a16366d0e4983e414339d5c3abfdf1
Your Name <you@example.com>
2023-02-24 00:54:14 -0800

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers, from 0 to 9
- *@n: Valid numbers to be checked
- * Return: 0.
- */
-
+ ** print_numbers - Prints the numbers from 0-9.
+ **/
 void print_numbers(void)
 {
-	int n;
+	int num;
 
-	for (n = 0; n < 9; n++)
-	{
-		putchar(n);
-	}
-	putchar(10);
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
