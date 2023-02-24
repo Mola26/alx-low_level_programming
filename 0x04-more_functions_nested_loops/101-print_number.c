824248d156ec33916add71966405b7337d39ac43
Your Name <you@example.com>
2023-02-24 08:15:01 -0800

    numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,13 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
- ** print_number - prints an integer
- ** @n: integer to print
- ** Return: void
- **/
-=======
  * print_number - prints an integer
  * @n: integer to print
  * Return: void
  */
->>>>>>> 3e8d988d255df672d937ba32eb754a56549890cb
 
 void print_number(int n)
 {
 	unsigned int num;
-<<<<<<< HEAD
-	if (n < 0)
-	{
-
-	_putchar('-');
-		num ==-n;
-	}
-	else
-=======
 
 	if (n < 0)
 	{
@@ -33,22 +17,14 @@ void print_number(int n)
 	}
 	else
 
->>>>>>> 3e8d988d255df672d937ba32eb754a56549890cb
 	{
 		num = n;
 	}
 
 	if (num / 10)
 	{
-<<<<<<< HEAD
-
-	print_number(num / 10);
-	}
-        	_putchar(num % 10 + '0');
-=======
 		print_number(num / 10);
 	}
 
 	_putchar(num % 10 + '0');
->>>>>>> 3e8d988d255df672d937ba32eb754a56549890cb
 }
