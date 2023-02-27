2d570473415f773191caf9aa3d8f8113679a06e6
Your Name <you@example.com>
2023-02-27 15:13:05 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+ #include "main.h"
+
+/**
+ * main - takes a pointer to an int as parameter and updates it points to to 98
+ * @n: variable integer
+ * Return: 0 Sucess
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	n = 98;
+
+}
