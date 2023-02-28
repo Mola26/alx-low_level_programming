ca78e60cf7aa666679e9d8e2b640247061dfe077
Your Name <you@example.com>
2023-02-27 17:25:30 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n element of an array of integers
+ * followed by a new line
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+		if (b != n - 1)
+		printf("%d, ", b[x]);
+		else
+			printf("%d", [b]);
+	printf("\n");
+}
