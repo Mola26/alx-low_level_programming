d24b14d8da412c4535cf67ec0608c7eb8513f829
Your Name <you@example.com>
2023-03-17 06:07:50 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- ** malloc_checked- cause normal process termination with a status value
- **
- ** @b: allocated memory
- **
- ** Return: a pointer to the allocated memory
+ * malloc_checked - cause normal process termination 
+ *
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
