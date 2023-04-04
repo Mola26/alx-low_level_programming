71e95274bf70530cff7ddff1f835ef90d1e204c5
Your Name <you@example.com>
2023-04-04 16:29:03 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,11 +7,39 @@
  * delete_nodeint_at_index - deletes the node at index
  * @head: pointer to the head
  * @index: index of the node that should be deleted
- * @n: name of the lits
  *
  * Return: 1 if succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-
+       	if (head == NULL || *head == NULL)
+	{
+		return (-1);
+	}
+	
+	if (index == 0)
+	{
+		listint_t *temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}listint_t *prev = *head;
+	listint_t *current = (*head)->next;
+	unsigned int i = 1;
+	
+	while (current != NULL && i < index)
+	{
+		prev = current;
+		current = current->next;
+		i++;
+	}
+	
+	if (current == NULL)
+	{
+		return (-1);
+	}
+	
+	prev->next = current->next;
+	free(current);
+	return (1);
 }
