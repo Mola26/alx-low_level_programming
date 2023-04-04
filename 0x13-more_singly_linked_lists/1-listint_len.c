ed2cc49dad7712a09f8aab4e60994cbc291a5cc4
Your Name <you@example.com>
2023-04-04 08:50:50 -0700

    returns list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "list.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_len list
+ * @h: name of the list
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
