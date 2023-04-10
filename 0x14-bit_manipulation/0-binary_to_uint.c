40ff4873c10732f07df6c3cc0680f60918b57081
Your Name <you@example.com>
2023-04-10 15:47:21 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int j = 0;
-	unsigned int i = 0;
+	unsigned int calc = 0;
 
 	if (b == NULL)
 		return (0);
-	while (b[j] != '\0')
+	while (b[j] != '0')
 	{
 		if (b[j] == '0')
-			i <<= 1;
-		else if (b[i] == '1')
-			i = (i << 1) | 1;
+			calc <<= 1;
+		else if (b[j] == '1')
+			calc = (calc << 1) | 1;
 		else
 			return (0);
 		j++;
 	}
-	return (j);
+	return (num);
 }
