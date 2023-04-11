99ac8e7b28573fdb497e3a364f574cbf149e6f2c
Your Name <you@example.com>
2023-04-11 13:39:05 -0700

    11 bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *set_bit - value of a bit to 1
  *@index: index starting from 0
@@ -6,12 +7,11 @@
  *
  * Return: 1 is worked or -1 if there is a error
  */
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
-	*n = ((1UL << index) | *n;
-			return (1);
+	*n = ((1UL << index) | *n);
+	return (1);
 }
