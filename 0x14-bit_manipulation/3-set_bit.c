3b19fe656fcd01e61f5f5670154e3e5f7847c545
Your Name <you@example.com>
2023-04-11 09:51:02 -0700

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,12 +9,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index);
 {
-	if (index >= sizeof(unsigned long int) * 8)
-	{
+	if (index > 63)
 		return (-1);
-	}
 
-	unsigned long int calc = 1UL << index;
-	*n | calc;
-	return (1);
+	*n = ((1UL << index) | *n;
+			return (1);
 }
