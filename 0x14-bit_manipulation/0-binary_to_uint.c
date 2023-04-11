3c2025cacb4a650af750220ce9d340991548e3fb
Your Name <you@example.com>
2023-04-11 00:09:15 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int j = 0;
 	unsigned int calc = 0;
 
 	if (b == NULL)
 		return (0);
-	while (b[j] != '0')
+
+	while (*b)
 	{
-		if (b[j] == '0')
-			calc <<= 1;
-		else if (b[j] == '1')
-			calc = (calc << 1) | 1;
-		else
+		if (b * != '0' && *b != '1')
+
 			return (0);
-		j++;
+		calc <<= 1;
+
+		if (*b++ == '1')
+			calc += 1;
 	}
-	return (num);
+	return (calc);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int calc = 1UL << ((sizeof(unsigned long int) * 8 - 1);
+	unsigned long int calc = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+	int d;
 
-		while (calc > 0)
-			{
-		_putchar((n & calc) ? '1' : '0';
+	while (calc > 0)
+	{
+		if (n & calc)
+			putchar('1');
+		else
+			putchar('0');
 		calc >>= 1;
-			}
+	}
 }
