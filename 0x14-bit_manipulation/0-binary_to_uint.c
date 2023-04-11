ca5e368ade0b7a7090a3d5f1598ef88f58a56b56
Your Name <you@example.com>
2023-04-11 09:26:17 -0700

    0.0 bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int n = 0;
+	unsigned int dec_value = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		n = 2 * n + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (n);
+	return (dec_val);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int_putchar (char c);
-int main(void);
+int _atoi(const char *s);
+int get_endianness(void);
 
 #endif
