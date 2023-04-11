7e0e095aa92f5c6ba8fc336e43d2fba30aa30bad
Your Name <you@example.com>
2023-04-11 09:06:29 -0700

    0.0 binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,23 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
+ * binary_to_uint -function that converts a binary number
  * @b: pointing to a string
  *
  * Return: 0 or NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int i = 0;
-	int len;
-	if (b == NULL)
-		return (0);
-	
-	len = strlen(b);
-	
-	for (i = 0; i < len; i++)
-	{
-		if (b[i] == '0')
-		{
-			num = num << 1;
-		}
-		else if (b[i] == '1')
-		{
-			num = (num << 1) | 1;
-		}
-		else
-	}
-	return (0);
+	int i;
+	unsigned int n = 0;
 
-}
-}
-return (num);
+	if(!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' | b[i] > '1')
+			return (0);
+		n = 2 * n + (b[i] - '0');
+	}
+	return (n);
 }
