9d5a4069a6d2e411e3850db4052367fa34721ef0
Your Name <you@example.com>
2023-04-13 15:59:23 -0700

    Copies wqFile descriptions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t f;
+	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
 	char *buf;
 
-	f = open(filename, O_RDONLY);
-	if (f == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
 	buf = malloc((sizeof(char) * letters);
-	t = read(f, buf, letters);
+	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
-	close(f);
+	close(fd);
 	return (w);
 }
